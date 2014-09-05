e929df490cc2795179addce21661be1360a2ebc3
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-09-04 20:06:45 -0500

    intent row1_button1

--- app/src/main/java/com/wyble/procesagro/MainActivity.java
+++ app/src/main/java/com/wyble/procesagro/MainActivity.java
@@ -1,21 +1,34 @@
 package com.wyble.procesagro;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener{
+    Button callView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callView= (Button) findViewById(R.id.row1_button1);
+        callView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("-->", "aqui es el intent");
+        //to do things!
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -32,5 +45,5 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
