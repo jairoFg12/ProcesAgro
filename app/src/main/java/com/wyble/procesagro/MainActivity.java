30ef4b131b99b002fab37e3513cf15be4cea7932
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-09-04 21:44:26 -0500

    webView working like a charm!

--- app/src/main/java/com/wyble/procesagro/MainActivity.java
+++ app/src/main/java/com/wyble/procesagro/MainActivity.java
@@ -1,35 +1,88 @@
 package com.wyble.procesagro;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity implements OnClickListener{
-    Button callView;
+public class MainActivity extends ActionBarActivity{
+    Button callView1;
+    Button callView2;
+    Button callView3;
+    Button callView4;
+    Button callView5;
+    Button callView6;
+    Button callView7;
+    Button callView8;
+    Button callView9;
+    Button callView10;
+
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        callView= (Button) findViewById(R.id.row1_button1);
-        callView.setOnClickListener(this);
+        callView1= (Button) findViewById(R.id.row1_button1);
+        callView2= (Button) findViewById(R.id.row1_button2);
+        callView3= (Button) findViewById(R.id.row2_button1);
+        callView4= (Button) findViewById(R.id.row2_button2);
 
-    }
+        callView5= (Button) findViewById(R.id.row3_button1);
+        callView6= (Button) findViewById(R.id.row2_button2);
 
-    @Override
-    public void onClick(View v) {
-        Log.d("-->", "aquí es el intent");
-        //to do things!
-        Uri uri = Uri.parse("http://www.google.com");
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+        callView7= (Button) findViewById(R.id.row2_button2);
+        callView8= (Button) findViewById(R.id.row2_button2);
+
+        callView9= (Button) findViewById(R.id.row2_button2);
+        callView10= (Button) findViewById(R.id.row2_button2);
+
+        callView1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("events", "clic boton 1");
+                String url1= "http://www.google.com";
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("URL_PARAMETER", url1);
+                startActivity(intent);
+            }
+        });
+
+        callView2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("events", "clic boton 2");
+                String url2= "http://www.siembra.gov.co/";
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("URL_PARAMETER", url2);
+                startActivity(intent);
+            }
+        });
+
+        callView3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("events", "clic boton 3");
+                String url3= "http://www.siembra.gov.co/";
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("URL_PARAMETER", url3);
+                startActivity(intent);
+            }
+        });
+
+        callView4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("events", "clic boton 4");
+                String url4= "http://www.corpoica.org.co/";
+                Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
+                intent.putExtra("URL_PARAMETER", url4);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     /*@Override
---
+++ app/src/main/java/com/wyble/procesagro/WebViewActivity.java
@@ -0,0 +1,45 @@
+package com.wyble.procesagro;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class WebViewActivity extends ActionBarActivity {
+    WebView webView1_aux;
+    String url_setter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        Bundle dataFromHomeActivity= getIntent().getExtras();
+        url_setter= dataFromHomeActivity.getString("URL_PARAMETER");
+
+        webView1_aux= (WebView) findViewById(R.id.webView1);
+        webView1_aux.loadUrl(url_setter);
+        webView1_aux.setWebViewClient(new WebViewClient());
+    }
+
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+}
