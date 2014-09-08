ad091724f070f7cb85e2b02327dbf761bd445320
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-09-07 22:59:02 -0500

    7Forms implanted

---
+++ app/src/main/java/com/wyble/procesagro/Call_Form2Activity.java
@@ -0,0 +1,14 @@
+package com.wyble.procesagro;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class Call_Form2Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call__form2);
+    }
+}
