480c3226ffc11fa96fba0d4ed6000163b900da8d
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-09-07 21:05:37 -0500

    IT WORKS!!!!

--- app/src/main/java/com/wyble/procesagro/CallActivity.java
+++ app/src/main/java/com/wyble/procesagro/CallActivity.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -36,20 +35,12 @@ public class CallActivity extends ActionBarActivity {
         convListView = (ListView) findViewById(R.id.convListView);
         ArrayAdapter<Convocatoria> arrayAdapter = new ArrayAdapter<Convocatoria>(this, R.layout.convocatoria_text_view, convocatorias);
         convListView.setAdapter(arrayAdapter);
-    }
 
-    private void listenerMethod() {
-        convListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                idregistro = (String) parentView.getItemAtPosition(position);
-                Log.d("id", idregistro);
-                Toast.makeText(context, "You seleceted " + idregistro + ".", Toast.LENGTH_LONG).show();
-            }
+        convListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-
+                Toast.makeText(getApplicationContext(),
+                        "" + position, Toast.LENGTH_SHORT).show();
             }
         });
     }
