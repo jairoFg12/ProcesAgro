5563db271f41c9928fb3eb38be3a8cb62f5bfbaa
David Alméciga <wdavid@dagrinchi.com>
2014-09-07 23:14:51 -0500

    Redes sociales

--- app/src/main/java/com/wyble/procesagro/ConvDetalle.java
+++ app/src/main/java/com/wyble/procesagro/ConvDetalle.java
@@ -2,7 +2,10 @@ package com.wyble.procesagro;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,8 +19,10 @@ import com.wyble.procesagro.models.Convocatoria;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ConvDetalle extends ActionBarActivity {
@@ -57,15 +62,45 @@ public class ConvDetalle extends ActionBarActivity {
 
         facebookBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                share("facebook", null, convocatoria.getUrl());
             }
         });
 
         twitterBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                share("twitter", null, convocatoria.getUrl());
             }
         });
 
+    }
+
+    private void share(String nameApp, String imagePath,String message) {
+        try
+        {
+            List<Intent> targetedShareIntents = new ArrayList<Intent>();
+            Intent share = new Intent(android.content.Intent.ACTION_SEND);
+            share.setType("image/jpeg");
+            List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(share, 0);
+            if (!resInfo.isEmpty()){
+                for (ResolveInfo info : resInfo) {
+                    Intent targetedShare = new Intent(android.content.Intent.ACTION_SEND);
+                    targetedShare.setType("image/jpeg"); // put here your mime type
+                    if (info.activityInfo.packageName.toLowerCase().contains(nameApp) || info.activityInfo.name.toLowerCase().contains(nameApp)) {
+                        targetedShare.putExtra(Intent.EXTRA_SUBJECT, "Sample Photo");
+                        targetedShare.putExtra(Intent.EXTRA_TEXT,message);
+                        targetedShare.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(imagePath)) );
+                        targetedShare.setPackage(info.activityInfo.packageName);
+                        targetedShareIntents.add(targetedShare);
+                    }
+                }
+                Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Select app to share");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[]{}));
+                startActivity(chooserIntent);
+            }
+        }
+
+        catch(Exception e){
+            Log.v("VM","Exception while sending image on" + nameApp + " "+  e.getMessage());
+        }
     }
 }
