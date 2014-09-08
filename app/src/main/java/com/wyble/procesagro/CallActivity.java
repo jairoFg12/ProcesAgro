1b8a4958a7819dff9e3993542ae06fe387222907
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-09-07 20:48:53 -0500

    onItemSelected ex method

--- app/src/main/java/com/wyble/procesagro/CallActivity.java
+++ app/src/main/java/com/wyble/procesagro/CallActivity.java
@@ -1,11 +1,16 @@
 package com.wyble.procesagro;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.wyble.procesagro.models.Convocatoria;
 
@@ -15,6 +20,8 @@ import java.util.ArrayList;
 public class CallActivity extends ActionBarActivity {
 
     private ListView convListView;
+    String idregistro;
+    final Context context = this;
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
@@ -29,7 +36,21 @@ public class CallActivity extends ActionBarActivity {
         convListView = (ListView) findViewById(R.id.convListView);
         ArrayAdapter<Convocatoria> arrayAdapter = new ArrayAdapter<Convocatoria>(this, R.layout.convocatoria_text_view, convocatorias);
         convListView.setAdapter(arrayAdapter);
-
     }
 
+    private void listenerMethod() {
+        convListView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                idregistro = (String) parentView.getItemAtPosition(position);
+                Log.d("id", idregistro);
+                Toast.makeText(context, "You seleceted " + idregistro + ".", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+        });
+    }
 }
