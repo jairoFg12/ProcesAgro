bd6648787461bf49bbb9bd219b1b2f6ce3f87680
David Alméciga <wdavid@dagrinchi.com>
2014-09-10 10:20:09 -0500

    Objeto de información de tramite

--- app/src/main/java/com/wyble/procesagro/Call_Form7Activity.java
+++ app/src/main/java/com/wyble/procesagro/Call_Form7Activity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -16,27 +17,29 @@ import java.io.Serializable;
 
 public class Call_Form7Activity extends ActionBarActivity {
 
-    private Button form7_send;
+    Button enviar;
     private EditText justificacion;
-    private Switch terminos;
+    private CheckBox terminos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call__form7);
 
-        Serializable dataFromPaso6 = getIntent().getSerializableExtra("TRAMITE_PASO6");
-        final Tramite tramite = (Tramite) dataFromPaso6;
+        //Serializable dataFromPaso6 = getIntent().getSerializableExtra("TRAMITE_PASO6");
+        //final Tramite tramite = (Tramite) dataFromPaso6;
 
-        justificacion = (EditText) findViewById(R.id.justificacion);
-        terminos = (Switch) findViewById(R.id.terminos);
+        //justificacion = (EditText) findViewById(R.id.justificacion);
+        //terminos = (CheckBox) findViewById(R.id.terminos);
 
-        form7_send = (Button) findViewById(R.id.form7_send);
-//        form7_send.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                //tramite.paso7(justificacion.getText().toString(), Boolean.parseBoolean(terminos.getText().toString()));
-//                Log.d(Call_Form7Activity.class.getName(), terminos.getText().toString());
-//            }
-//        });
+        enviar = (Button) findViewById(R.id.finish_butt);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("->", "el click");
+                //tramite.paso7(justificacion.getText().toString(), Boolean.parseBoolean(terminos.getText().toString()));
+                //Log.d(Call_Form7Activity.class.getName(), terminos.getText().toString());
+            }
+        });
     }
 }
