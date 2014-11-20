2714c4fee45f0985b7d312ceff83b3e85a5b40e7
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-11-20 00:22:59 -0500

    Listado Departamentos y Municipios
    Retirado el check box de condiciones y legal

--- app/src/main/java/com/wyble/procesagro/Call_Form2Activity.java
+++ app/src/main/java/com/wyble/procesagro/Call_Form2Activity.java
@@ -4,9 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.wyble.procesagro.helpers.DB;
@@ -20,7 +21,8 @@ import java.util.HashMap;
 public class Call_Form2Activity extends ActionBarActivity {
 
     private Button form2_next;
-    private EditText municipio, departamento;
+    //private EditText municipio, departamento;
+    private Spinner municipio, departamento;
     Context context= this;
     private static final String TRAMITE_TABLE = "tramites";
 
@@ -32,20 +34,25 @@ public class Call_Form2Activity extends ActionBarActivity {
         Serializable dataFromPaso1 = getIntent().getSerializableExtra("TRAMITE_PASO1");
         final Tramite tramite = (Tramite) dataFromPaso1;
 
-        municipio = (EditText) findViewById(R.id.municipio);
-        departamento = (EditText) findViewById(R.id.departamento);
+        municipio = (Spinner) findViewById(R.id.municipio);
+        departamento = (Spinner) findViewById(R.id.departamento);
 
-        municipio.setText(tramite.getMunicipio());
-        departamento.setText(tramite.getDepartamento());
+        //municipio.setText(tramite.getMunicipio());
+        //departamento.setText(tramite.getDepartamento());
+        //municipio.setText(tramite.getMunicipio());
+        //departamento.setText(tramite.getDepartamento());
 
         form2_next = (Button) findViewById(R.id.form2_next);
         form2_next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                final String municipio_v= municipio.getText().toString().trim();
-                final String departamento_v= departamento.getText().toString().trim();
-                if(municipio_v.equals("") || departamento_v.equals("")){
+                //final String municipio_v = municipio.getText().toString().trim();
+                //final String departamento_v = departamento.getText().toString().trim();
+                String municipio_v = municipio.getSelectedItem().toString();
+                String departamento_v = departamento.getSelectedItem().toString();
+                Log.d("//log", "//log" + municipio_v + " - " + departamento_v);
 
-                    Toast.makeText(context, "Todos los campos son requeridos.", Toast.LENGTH_SHORT).show();
+                if(municipio_v.equals("Seleccione un municipio") || departamento_v.equals("Seleccione un departamento")){
+                    Toast.makeText(context, "Seleccione un departamento y municipio.", Toast.LENGTH_SHORT).show();
                 }else{
                     tramite.paso2(municipio_v, departamento_v);
 
